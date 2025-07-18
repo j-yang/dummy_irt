29bf9c148003b1d4aaa843647b383218d720714e
Jimmy Yang <ktxv525@astrazeneca.com>
2025-07-18 11:31:24 +0800

    Setup GitHub Pages deployment with project manager

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/dummy_irt/' : '/',
   plugins: [
     vue(),
     VitePWA({
